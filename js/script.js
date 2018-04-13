4ac26f9bb7b843385c3f3321b041ec7f74a31744
Raquel <raquelaguilar@ciencias.unam.mx>
2018-04-13 03:08:47 -0500

    section anatomia

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
 	$("#first-title").on("click", function(){
 		$("#info-banner").toggleClass("open");
+	});
+	$("#sub-menu-icon").find("img").on("click", function() {
+		$(".sub-menu-opts").toggleClass("open");
+		$(".up").toggle();
+		$(".down").toggle();
 	})
 })
