eaa653424341a07b2e6c907c950c360cf5c2f1b4
RaquelAM <raquelaguilar@ciencias.unam.mx>
2018-04-27 11:22:12 -0500

    remove useless functions

--- js/script.js
+++ js/script.js
@@ -31,9 +31,7 @@ $(document).ready(function(){
 
 
     /***SISMO***/
-    $("#delegacion").addClass("active");
-    $("#suelo").removeClass("active");
-    console.log("click");
+    
 	$("#imgArrows").hide();	
 	$("#first-title").on("click", function(){
 		$("#info-banner").toggleClass("open");
@@ -128,117 +126,5 @@ $(document).ready(function(){
 		$('#appendImg').prepend($('<img>',{class: 'imageCol', src:'../img/sismo/ley/colonias/'+col}))
     });
 
-    $("#delegacion").on("click",function(){
-    	$(this).addClass("active");
-    	$("#suelo").removeClass("active");
-    	$("#all").html("<img class='separadores' src='../img/sismo/ley/separadores/delbj.svg' />\
-    		<img src='../img/sismo/ley/edificios/avninosheroes173.bj.svg' />\
-    		<img src='../img/sismo/ley/edificios/balsas18.bj.svg' />\
-    		<img src='../img/sismo/ley/edificios/bretana90.bj.svg' />\
-    		<img src='../img/sismo/ley/edificios/concepcionbesitegui1509.bj.svg' />\
-    		<img src='../img/sismo/ley/edificios/edimburgo4.bj.svg' />\
-    		<img src='../img/sismo/ley/edificios/emilianozapata56.bj.svg' />\
-    		<img src='../img/sismo/ley/edificios/enriquerebsamen241.bj.svg' />\
-    		<img src='../img/sismo/ley/edificios/escocia4.bj.svg' />\
-    		<img src='../img/sismo/ley/edificios/patriciosanz37.bj.svg' />\
-    		<img src='../img/sismo/ley/edificios/prolpeten915.bj.svg' />\
-    		<img src='../img/sismo/ley/edificios/saratoga714.bj.svg' />\
-    		<img src='../img/sismo/ley/edificios/tokio517.bj.svg' />\
-    		<img src='../img/sismo/ley/edificios/viaductomiguelaleman106.bj.svg' />\
-    		<img src='../img/sismo/ley/edificios/xola32.bj.svg' />\
-    		<img class='separadores' src='../img/sismo/ley/separadores/delco.svg' />\
-    		<img src='../img/sismo/ley/edificios/avsantaana300.co.svg' />\
-    		<img src='../img/sismo/ley/edificios/caltlalpan2050.co.svg' />\
-    		<img src='../img/sismo/ley/edificios/calhueso480.co.svg' />\
-    		<img src='../img/sismo/ley/edificios/canalmiramontes2747.co.svg' />\
-    		<img src='../img/sismo/ley/edificios/cuauhtemoc75.co.svg' />\
-    		<img src='../img/sismo/ley/edificios/edmultifamiliar.co.svg' />\
-    		<img src='../img/sismo/ley/edificios/escocia29.co.svg' />\
-    		<img src='../img/sismo/ley/edificios/escocia33.co.svg' />\
-    		<img src='../img/sismo/ley/edificios/rancholosarcos32.co.svg' />\
-    		<img class='separadores' src='../img/sismo/ley/separadores/delcu.svg' />\
-    		<img src='../img/sismo/ley/edificios/alvarobregon284.cu.svg' />\
-    		<img src='../img/sismo/ley/edificios/alvarobregon286.cu.svg' />\
-    		<img src='../img/sismo/ley/edificios/amsterdam25.cu.svg' />\
-    		<img src='../img/sismo/ley/edificios/amsterdam107.cu.svg' />\
-    		<img src='../img/sismo/ley/edificios/bolivar168.cu.svg' />\
-    		<img src='../img/sismo/ley/edificios/calsanantonioabad122.cu.svg' />\
-    		<img src='../img/sismo/ley/edificios/chihuahua110.cu.svg' />\
-    		<img src='../img/sismo/ley/edificios/coahuila8.cu.svg' />\
-    		<img src='../img/sismo/ley/edificios/genova33.cu.svg' />\
-    		<img src='../img/sismo/ley/edificios/magnolia76.cu.svg' />\
-    		<img src='../img/sismo/ley/edificios/medellin176.cu.svg' />\
-    		<img src='../img/sismo/ley/edificios/mina16.cu.svg' />\
-    		<img src='../img/sismo/ley/edificios/puebla282.cu.svg' />\
-    		<img src='../img/sismo/ley/edificios/queretaro222.cu.svg' />\
-    		<img src='../img/sismo/ley/edificios/salvadordmiron155.cu.svg' />\
-    		<img src='../img/sismo/ley/edificios/sonora149.cu.svg' />\
-    		<img class='separadores' src='../img/sismo/ley/separadores/delga.svg' />\
-    		<img src='../img/sismo/ley/edificios/calle323655.ga.svg' />\
-    		<img src='../img/sismo/ley/edificios/coquimbo911.ga.svg' />\
-    		<img class='separadores' src='../img/sismo/ley/separadores/deli.svg' />\
-    		<img src='../img/sismo/ley/edificios/paseodelasgalias47.i.svg' />\
-    		<img class='separadores' src='../img/sismo/ley/separadores/delt.svg' />\
-    		<img src='../img/sismo/ley/edificios/puente222.t.svg' />\
-    		<img class='separadores' src='../img/sismo/ley/separadores/delx.svg' />\
-    		<img src='../img/sismo/ley/edificios/ranchotamboreo11.t.svg' />\
-    		<img src='../img/sismo/ley/edificios/avmexico1.x.svg' />\
-    		<img src='../img/sismo/ley/edificios/avmexicoponiente47.x.svg' />\
-    		");
-    });
-
-    $("#suelo").on("click",function(){
-    	$(this).addClass("active");
-    	$("#delegacion").removeClass("active");
-    	$("#all").html("<img class='separadores' src='../img/sismo/ley/separadores/lacustre.svg' />\
-    		<img src='../img/sismo/ley/edificios/avninosheroes173.bj.svg' />\
-    		<img src='../img/sismo/ley/edificios/balsas18.bj.svg' />\
-    		<img src='../img/sismo/ley/edificios/bretana90.bj.svg' />\
-    		<img src='../img/sismo/ley/edificios/concepcionbesitegui1509.bj.svg' />\
-    		<img src='../img/sismo/ley/edificios/emilianozapata56.bj.svg' />\
-    		<img src='../img/sismo/ley/edificios/enriquerebsamen241.bj.svg' />\
-    		<img src='../img/sismo/ley/edificios/prolpeten915.bj.svg' />\
-    		<img src='../img/sismo/ley/edificios/saratoga714.bj.svg' />\
-    		<img src='../img/sismo/ley/edificios/tokio517.bj.svg' />\
-    		<img src='../img/sismo/ley/edificios/viaductomiguelaleman106.bj.svg' />\
-    		<img src='../img/sismo/ley/edificios/avsantaana300.co.svg' />\
-    		<img src='../img/sismo/ley/edificios/calhueso480.co.svg' />\
-    		<img src='../img/sismo/ley/edificios/canalmiramontes2747.co.svg' />\
-    		<img src='../img/sismo/ley/edificios/escocia29.co.svg' />\
-    		<img src='../img/sismo/ley/edificios/escocia33.co.svg' />\
-    		<img src='../img/sismo/ley/edificios/rancholosarcos32.co.svg' />\
-    		<img src='../img/sismo/ley/edificios/alvarobregon284.cu.svg' />\
-    		<img src='../img/sismo/ley/edificios/alvarobregon286.cu.svg' />\
-    		<img src='../img/sismo/ley/edificios/amsterdam25.cu.svg' />\
-    		<img src='../img/sismo/ley/edificios/amsterdam107.cu.svg' />\
-    		<img src='../img/sismo/ley/edificios/bolivar168.cu.svg' />\
-    		<img src='../img/sismo/ley/edificios/calsanantonioabad122.cu.svg' />\
-    		<img src='../img/sismo/ley/edificios/chihuahua110.cu.svg' />\
-    		<img src='../img/sismo/ley/edificios/coahuila8.cu.svg' />\
-    		<img src='../img/sismo/ley/edificios/genova33.cu.svg' />\
-    		<img src='../img/sismo/ley/edificios/magnolia76.cu.svg' />\
-    		<img src='../img/sismo/ley/edificios/medellin176.cu.svg' />\
-    		<img src='../img/sismo/ley/edificios/mina16.cu.svg' />\
-    		<img src='../img/sismo/ley/edificios/puebla282.cu.svg' />\
-    		<img src='../img/sismo/ley/edificios/queretaro222.cu.svg' />\
-    		<img src='../img/sismo/ley/edificios/salvadordmiron155.cu.svg' />\
-    		<img src='../img/sismo/ley/edificios/sonora149.cu.svg' />\
-    		<img src='../img/sismo/ley/edificios/calle323655.ga.svg' />\
-    		<img src='../img/sismo/ley/edificios/coquimbo911.ga.svg' />\
-    		<img src='../img/sismo/ley/edificios/ranchotamboreo11.t.svg' />\
-    		<img class='separadores' src='../img/sismo/ley/separadores/transicion.svg' />\
-    		<img src='../img/sismo/ley/edificios/escocia4.bj.svg' />\
-    		<img src='../img/sismo/ley/edificios/edimburgo4.bj.svg' />\
-    		<img src='../img/sismo/ley/edificios/patriciosanz37.bj.svg' />\
-    		<img src='../img/sismo/ley/edificios/xola32.bj.svg' />\
-    		<img src='../img/sismo/ley/edificios/caltlalpan2050.co.svg' />\
-    		<img src='../img/sismo/ley/edificios/cuauhtemoc75.co.svg' />\
-    		<img src='../img/sismo/ley/edificios/edmultifamiliar.co.svg' />\
-    		<img src='../img/sismo/ley/edificios/paseodelasgalias47.i.svg' />\
-    		<img src='../img/sismo/ley/edificios/puente222.t.svg' />\
-    		<img src='../img/sismo/ley/edificios/avmexico1.x.svg' />\
-    		<img src='../img/sismo/ley/edificios/avmexicoponiente47.x.svg' />\
-    		");
-    });
 		
 });
