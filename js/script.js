953ca101078c4fdd8fe74b6863f8f018f5cc6119
RaquelAM <raquelaguilar@ciencias.unam.mx>
2018-06-12 15:12:19 -0500

    function randome position circles dataPinta

--- js/script.js
+++ js/script.js
@@ -37,6 +37,13 @@ $(document).ready(function(){
   $("#cerrarPinta").on("click", function(){
     window.history.back();
   });
+  var max = $("#containerPintaSection").height() -250;
+  $.each( $('.containerCircle'), function(i, cont) {
+     $('.circlePinta', cont).each(function(i, elem) {
+          var top = max * Math.random();
+          $(elem).css("top", top);
+     });
+  })
 /**Quienes somos**/
   $("#myRange").val(1);
   $("#myRange").on("change", function(){
